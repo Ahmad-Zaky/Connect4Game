9cfc6e1bd3ba77a322e09fccd77a83b403095814
Ahmad-Zaky <44299134+Ahmad-Zaky@users.noreply.github.com>
2019-10-18 07:48:35 +0200

    play with 2nd player

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -2,3 +2,4 @@
 // It is a c++ programm to illustrate the game
 // we buil up 4 main functions for the game 
 // our Game contains 2 options play with Pc or with 2nd Player
+// here we start the Game play with 2nd player
