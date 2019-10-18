0f83842b575ba2e7a094e076b9ede13d311d4889
Ahmed_Zaky <ahmed6mohamed6@gmail.com>
2019-10-18 18:56:28 +0200

    Cmder commit try

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -5,6 +5,6 @@
 // our Game contains 2 options play with Pc or with 2nd Player
 // we start here the Game play with 2nd player
 
-// we type here the Game play with Pc
+// we type here the Game play with the Pc
 
 // now I add more lines to for our commit locally
