5ddfe84c870ee5835d32ee8914391a80924bbe85
Ahmad-Zaky <44299134+Ahmad-Zaky@users.noreply.github.com>
2019-10-18 07:43:29 +0200

    Game Options

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -1,3 +1,4 @@
 // Welcome to Connect4Game
 // It is a c++ programm to illustrate the game
 // we buil up 4 main functions for the game 
+// our Game contains 2 options play with Pc or with 2nd Player
