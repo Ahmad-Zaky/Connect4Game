e34a1320d48a485af5591c81eb28e829afceeed1
Ahmed_Zaky <ahmed6mohamed6@gmail.com>
2019-10-19 20:12:43 +0200

    try to merge them together

--- Connect4Game.cpp
+++ Connect4Game.cpp
@@ -7,7 +7,7 @@
 
 // we type here the Game play with the Pc
 
-// now I add more lines to for our commit locally
+// now I add a lot of lines to for our commit locally
 
 // adding a new line for our new branch called " new_feature "
 
